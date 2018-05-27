d7d5bc07e4ab632182cf1de672323a421d3856bd
lianglirong <947491240@qq.com>
2018-05-27 10:51:33 +0800

    添加注册

--- server/src/http_msg_server/HttpConn.cpp
+++ server/src/http_msg_server/HttpConn.cpp
@@ -176,7 +176,7 @@ void CHttpConn::OnRead()
 	if (m_HttpParser.IsReadAll()) {
 		string url =  m_HttpParser.GetUrl();
 
-		log("CHttpConn url:",url);
+		log("CHttpConn url: %s",url);
 		if (strncmp(url.c_str(), "/query/", 7) == 0) {
 			string content = m_HttpParser.GetBodyContent();
 			CHttpQuery* pQueryInstance = CHttpQuery::GetInstance();
